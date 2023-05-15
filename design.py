3e87f5b43d72ea4ff880cd85a9dae44c3a208882
Stas <130368840+d666dizlayk@users.noreply.github.com>
2023-05-15 09:17:52 +0300

    Add files via upload

--- design.py
+++ design.py
@@ -13,91 +13,109 @@ from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
 
-class Ui_mainWindow(object):
-    def setupUi(self, mainWindow):
-        if not mainWindow.objectName():
-            mainWindow.setObjectName(u"mainWindow")
-        mainWindow.resize(878, 265)
-        mainWindow.setMinimumSize(QSize(878, 262))
-        mainWindow.setMaximumSize(QSize(880, 265))
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(465, 440)
+        MainWindow.setMinimumSize(QSize(465, 440))
+        MainWindow.setMaximumSize(QSize(465, 440))
         font = QFont()
-        font.setPointSize(15)
-        mainWindow.setFont(font)
-        self.centralwidget = QWidget(mainWindow)
+        font.setPointSize(12)
+        MainWindow.setFont(font)
+        self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.a_line = QLineEdit(self.centralwidget)
-        self.a_line.setObjectName(u"a_line")
-        self.a_line.setGeometry(QRect(10, 50, 201, 30))
-        self.label = QLabel(self.centralwidget)
+        self.widget = QWidget(self.centralwidget)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(50, 20, 363, 336))
+        self.verticalLayout = QVBoxLayout(self.widget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel(self.widget)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(10, 20, 11, 24))
-        self.b_line = QLineEdit(self.centralwidget)
-        self.b_line.setObjectName(u"b_line")
-        self.b_line.setGeometry(QRect(130, 50, 201, 30))
-        self.label_2 = QLabel(self.centralwidget)
+
+        self.verticalLayout.addWidget(self.label)
+
+        self.coef_edit = QLineEdit(self.widget)
+        self.coef_edit.setObjectName(u"coef_edit")
+
+        self.verticalLayout.addWidget(self.coef_edit)
+
+        self.label_2 = QLabel(self.widget)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(130, 20, 11, 24))
-        self.c_line = QLineEdit(self.centralwidget)
-        self.c_line.setObjectName(u"c_line")
-        self.c_line.setGeometry(QRect(260, 50, 201, 30))
-        self.label_3 = QLabel(self.centralwidget)
+        self.label_2.setLayoutDirection(Qt.LeftToRight)
+        self.label_2.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_2)
+
+        self.eps_edit = QLineEdit(self.widget)
+        self.eps_edit.setObjectName(u"eps_edit")
+
+        self.verticalLayout.addWidget(self.eps_edit)
+
+        self.label_3 = QLabel(self.widget)
         self.label_3.setObjectName(u"label_3")
-        self.label_3.setGeometry(QRect(260, 20, 9, 24))
-        self.g_line = QLineEdit(self.centralwidget)
-        self.g_line.setObjectName(u"g_line")
-        self.g_line.setGeometry(QRect(400, 50, 201, 30))
-        self.h_line = QLineEdit(self.centralwidget)
-        self.h_line.setObjectName(u"h_line")
-        self.h_line.setGeometry(QRect(530, 50, 201, 30))
-        self.label_4 = QLabel(self.centralwidget)
+
+        self.verticalLayout.addWidget(self.label_3)
+
+        self.left_edit = QLineEdit(self.widget)
+        self.left_edit.setObjectName(u"left_edit")
+
+        self.verticalLayout.addWidget(self.left_edit)
+
+        self.label_4 = QLabel(self.widget)
         self.label_4.setObjectName(u"label_4")
-        self.label_4.setGeometry(QRect(410, 20, 11, 24))
-        self.label_5 = QLabel(self.centralwidget)
-        self.label_5.setObjectName(u"label_5")
-        self.label_5.setGeometry(QRect(530, 20, 11, 24))
-        self.n_line = QLineEdit(self.centralwidget)
-        self.n_line.setObjectName(u"n_line")
-        self.n_line.setGeometry(QRect(660, 50, 201, 30))
-        self.label_6 = QLabel(self.centralwidget)
-        self.label_6.setObjectName(u"label_6")
-        self.label_6.setGeometry(QRect(660, 20, 11, 24))
-        self.simpson_button = QPushButton(self.centralwidget)
-        self.simpson_button.setObjectName(u"simpson_button")
-        self.simpson_button.setGeometry(QRect(20, 100, 181, 32))
-        self.sp_button = QPushButton(self.centralwidget)
-        self.sp_button.setObjectName(u"sp_button")
-        self.sp_button.setGeometry(QRect(260, 100, 321, 32))
-        self.simpson_label = QLabel(self.centralwidget)
-        self.simpson_label.setObjectName(u"simpson_label")
-        self.simpson_label.setGeometry(QRect(20, 140, 231, 24))
-        self.sp_label = QLabel(self.centralwidget)
-        self.sp_label.setObjectName(u"sp_label")
-        self.sp_label.setGeometry(QRect(260, 140, 271, 24))
-        mainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QMenuBar(mainWindow)
+
+        self.verticalLayout.addWidget(self.label_4)
+
+        self.right_edit = QLineEdit(self.widget)
+        self.right_edit.setObjectName(u"right_edit")
+
+        self.verticalLayout.addWidget(self.right_edit)
+
+        self.pdButton = QPushButton(self.widget)
+        self.pdButton.setObjectName(u"pdButton")
+
+        self.verticalLayout.addWidget(self.pdButton)
+
+        self.pd_Label = QLabel(self.widget)
+        self.pd_Label.setObjectName(u"pd_Label")
+
+        self.verticalLayout.addWidget(self.pd_Label)
+
+        self.ntButton = QPushButton(self.widget)
+        self.ntButton.setObjectName(u"ntButton")
+
+        self.verticalLayout.addWidget(self.ntButton)
+
+        self.nt_label = QLabel(self.widget)
+        self.nt_label.setObjectName(u"nt_label")
+
+        self.verticalLayout.addWidget(self.nt_label)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 878, 34))
-        mainWindow.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(mainWindow)
+        self.menubar.setGeometry(QRect(0, 0, 465, 27))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
-        mainWindow.setStatusBar(self.statusbar)
+        MainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(mainWindow)
+        self.retranslateUi(MainWindow)
 
-        QMetaObject.connectSlotsByName(mainWindow)
+        QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
-    def retranslateUi(self, mainWindow):
-        mainWindow.setWindowTitle(QCoreApplication.translate("mainWindow", u"calculator", None))
-        self.label.setText(QCoreApplication.translate("mainWindow", u"a", None))
-        self.label_2.setText(QCoreApplication.translate("mainWindow", u"b", None))
-        self.label_3.setText(QCoreApplication.translate("mainWindow", u"c", None))
-        self.label_4.setText(QCoreApplication.translate("mainWindow", u"g", None))
-        self.label_5.setText(QCoreApplication.translate("mainWindow", u"h", None))
-        self.label_6.setText(QCoreApplication.translate("mainWindow", u"n", None))
-        self.simpson_button.setText(QCoreApplication.translate("mainWindow", u"\u043c\u0435\u0442\u043e\u0434 \u0421\u0438\u043c\u043f\u0441\u043e\u043d\u0430", None))
-        self.sp_button.setText(QCoreApplication.translate("mainWindow", u"\u043c\u0435\u0442\u043e\u0434 \u0441\u0440\u0435\u0434\u043d\u0438\u0445 \u043f\u0440\u044f\u043c\u043e\u0443\u0433\u043e\u043b\u044c\u043d\u0438\u043a\u043e\u0432", None))
-        self.simpson_label.setText("")
-        self.sp_label.setText("")
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"\u043d\u0435\u043b\u0438\u043d\u0435\u0439\u043d\u043e\u0435 \u0443\u0440\u0430\u0432\u043d\u0435\u043d\u0438\u0435 \u0441 \u043f\u043e\u043b\u0438\u043d\u043e\u043c\u043e\u043c \u0434\u0432\u0435\u043d\u0430\u0434\u0446\u0430\u0442\u043e\u0439 \u0441\u0442\u0435\u043f\u0435\u043d\u0438", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"\u0412\u0432\u0435\u0434\u0438\u0442\u0435 \u043a\u043e\u044d\u0444\u0444\u0438\u0446\u0438\u0435\u043d\u0442\u044b \u0443\u0440\u0430\u0432\u043d\u0435\u043d\u0438\u044f \u0447\u0435\u0440\u0435\u0437 \u043f\u0440\u043e\u0431\u0435\u043b", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u0412\u0432\u0435\u0434\u0438\u0442\u0435 \u0437\u0430\u0434\u0430\u043d\u043d\u0443\u044e \u0442\u043e\u0447\u043d\u043e\u0441\u0442\u044c \u0432\u044b\u0447\u0438\u0441\u043b\u0435\u043d\u0438\u044f", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"\u041d\u0430\u0447\u0430\u043b\u043e \u0438\u043d\u0442\u0435\u0440\u0432\u0430\u043b\u0430:", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"\u041a\u043e\u043d\u0435\u0446 \u0438\u043d\u0442\u0435\u0440\u0432\u0430\u043b\u0430:", None))
+        self.pdButton.setText(QCoreApplication.translate("MainWindow", u"\u041c\u0435\u0442\u043e\u0434 \u043f\u043e\u043b\u043e\u0432\u0438\u043d\u043d\u043e\u0433\u043e \u0434\u0435\u043b\u0435\u043d\u0438\u044f", None))
+        self.pd_Label.setText(QCoreApplication.translate("MainWindow", u"\u0420\u0435\u0437\u0443\u043b\u044c\u0442\u0430\u0442: ", None))
+        self.ntButton.setText(QCoreApplication.translate("MainWindow", u"\u041c\u0435\u0442\u043e\u0434 \u041d\u044c\u044e\u0442\u043e\u043d\u0430", None))
+        self.nt_label.setText(QCoreApplication.translate("MainWindow", u"\u0420\u0435\u0437\u0443\u043b\u044c\u0442\u0430\u0442: ", None))
     # retranslateUi
 
--- main.py
+++ main.py
@@ -1,69 +1,91 @@
 from PySide2.QtWidgets import QMainWindow, QApplication
-from design import Ui_mainWindow
-
-def sp(a, b, c, g, h, n):
-    dx = (h - g) / n
-    integral = 0
-    for i in range(n):
-        x0 = g + i * dx
-        x1 = g + (i + 1) * dx
-        xi = (x0 + x1) / 2
-        fi = a * xi ** b + c
-        integral += fi * dx
-    return integral
+from design import Ui_MainWindow
+from math import pow, fabs
 
 class calculator(QMainWindow):
     def __init__(self,parent=None):
         super(calculator, self).__init__(parent)
-        self.ui = Ui_mainWindow()
+        self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.sp_button.clicked.connect(self.run_sp)
-        self.ui.simpson_button.clicked.connect(self.run_simpson)
+        self.ui.pdButton.clicked.connect(self.pd_run)
+        self.ui.ntButton.clicked.connect(self.newton_run)
 
-    def run_sp(self):
-        a = float(self.ui.a_line.text())
-        b = float(self.ui.b_line.text())
-        c = float(self.ui.c_line.text())
-        g = float(self.ui.g_line.text())
-        h = float(self.ui.h_line.text())
-        n = float(self.ui.n_line.text())
-        root = sp(a,b,c,g,h,n)
-        self.ui.sp_label.setText(f'Корень: {root:.6f}')
+    def pd_run(self):
+        coef = list(map(float, self.ui.coef_edit.text().split(' ')))
+        eps = float(self.ui.eps_edit.text())
+        left = float(self.ui.left_edit.text())
+        right = float(self.ui.right_edit.text())
 
-    def run_simpson(self):
-        a = float(self.ui.a_line.text())
-        b = float(self.ui.b_line.text())
-        c = float(self.ui.c_line.text())
-        g = float(self.ui.g_line.text())
-        h = float(self.ui.h_line.text())
-        n = float(self.ui.n_line.text())
-        root = simpson(a,b,c,g,h,n)
-        self.ui.simpson_label.setText(f'Корень: {root:.6f}')
+        root = poldel(left,right,eps,coef)
+        self.ui.pd_Label.setText(f'Корень: {root:.6f}')
+
+    def newton_run(self):
+        coef = list(map(float, self.ui.coef_edit.text().split(' ')))
+        eps = float(self.ui.eps_edit.text())
+        left = float(self.ui.left_edit.text())
+        right = float(self.ui.right_edit.text())
+
+        root = newton(left,coef,eps,1000)
+        self.ui.nt_label.setText(f'Корень: {root:.6f}')
+
+def f(x, coef):
+    """ Вычисление значения полинома на заданной точке """
+    # Задайте полином и его коэффициенты
+    a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12 = coef
+    fx = a0 + a1 * x + a2 * pow(x, 2) + a3 * pow(x, 3) + a4 * pow(x, 4) + a5 * pow(x, 5) + a6 * pow(x, 6) + a7 * pow(x, 7) + a8 * pow(x, 8) + a9 * pow(x, 9) + a10 * pow(x, 10) + a11 * pow(x, 11) + a12 * pow(x, 12)
+    return fx
 
 
-def sp(a,b,c,g,h,n):
-    dx = (h - g) / n
-    integral = 0
-    for i in range(int(n)):
-        x0 = g + i * dx
-        x1 = g + (i + 1) * dx
-        xi = (x0 + x1) / 2
-        fi = a * xi ** b + c
-        integral += fi * dx
-    return integral
+def f_prim(x, coef):
+    """ Вычисление значения производной полинома в заданной точке"""
+    a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12 = coef
+    f_prim = a1 + 2 * a2 * x + 3 * a3 * pow(x, 2) + 4 * a4 * pow(x, 3) + 5 * a5 * pow(x, 4) + 6 * a6 * pow(x, 5) + 7 * a7 * pow(x, 6) + 8 * a8 * pow(x, 7) + 9 * a9 * pow(x, 8) + 10 * a10 * pow(x, 9) + 11 * a11 * pow(x, 10) + 12 * a12 * pow(x, 11)
+    return f_prim
 
-def simpson(a, b, c, g, h, n):
-    dx = (h - g) / n
-    integral = 0
-    for i in range(int(n)):
-        x0 = g + i * dx
-        x1 = g + (i + 1) * dx
-        xi = (x0 + x1) / 2
-        fi0 = a * x0 ** b + c
-        fi1 = a * x1 ** b + c
-        fxi = a * xi ** b + c
-        integral += dx / 6 * (fi0 + 4*fxi + fi1)
-    return integral
+# def poldel(left, right, coef, eps):
+#     """ Решение уравнения методом половинного деления (бинарный поиск) с заданной точностью """
+#     center = (left + right) / 2
+#     while fabs(f(center, coef)) > eps:
+#         if f(left, coef) * f(center, coef) < 0:
+#             right = center
+#         else:
+#             left = center
+#
+#         center = (left + right) / 2
+#
+#     return center
+def poldel(left, right, eps,coef):
+    mid = (left + right) / 2.0
+    while abs(f(mid,coef)) > eps:
+        if f(mid,coef) == 0:
+            return mid
+        elif f(left,coef) * f(mid,coef) < 0:
+            right = mid
+        else:
+            left = mid
+        mid = (left + right) / 2.0
+    return mid
+
+#def newton(x0, coef, eps):
+#    """ Решение уравнения методом Ньютона с заданной точностью """
+#    x = x0
+#    while abs(f(x, coef)) > eps:
+#        x -= f(x, coef) / f_prim(x, coef)
+#    return x
+# max_iter=1000
+def newton(left, coef, eps, max_iter):
+    xn = left
+
+    for n in range(0, max_iter):
+        fxn = f(xn,coef)
+        if abs(fxn) < eps:
+            return xn
+        Dfxn = f_prim(xn,coef)
+        if Dfxn == 0:
+            return None
+        xn = xn - fxn / Dfxn
+
+    return None
 
 if __name__ == '__main__':
     app = QApplication([])
